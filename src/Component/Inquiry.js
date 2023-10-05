2d1492b6d12bbf32ceed2cc7f014a587ceb56dfc
darshakkanani2002 <139429390+darshakkanani2002@users.noreply.github.com>
2023-10-05 12:06:43 +0530

    Update Inquiry.js

--- src/Component/Inquiry.js
+++ src/Component/Inquiry.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react'
+import Styled from 'styled-components';
 
 export default class Inquiry extends Component {
+    
+
     render() {
+        let ButtonStyle = Styled.button`
+        background-color:#0698d6;
+        padding:10px;
+        color:#fff;
+        border:none;
+        font-weight:bold;
+        border-radius:8px;
+        &&:hover{
+          background-color:#000;
+          transition:0.5s;
+        }`
         return (
             <div>
                 <div className='about-text-div'>
@@ -13,6 +27,39 @@ export default class Inquiry extends Component {
                             <h3 className='fw-bold inquiry-first-back-text'>DON’T KNOW WHAT TO START WITH?</h3>
                             <h1 className='text-light fw-bolder mt-5'>PLEASE FILL IN THE FOLLOWING FORM<br /> AND WE’LL GET BACK TO YOU <br /> SHORTLY
                             </h1>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <div className='container p-5 inquiry-form-div'>
+                        <div className='row'>
+                            <div className='col-lg-6 border'>
+                                <h5 className='text-muted pt-4 '>FREE CONSULTATION</h5>
+                                <h2>GET A FREE QUOTE</h2>
+
+                                <div className='container pb-4'>
+                                    <form onSubmit={this.onSubmit}>
+                                        <label className='form-lable mt-3'>Name*</label>
+                                        <input type='text' className='form-control'></input>
+
+                                        <label className='form-lable mt-3'>Email*</label>
+                                        <input type='email' className='form-control'></input>
+
+                                        <label className='form-lable mt-3'>Subject*</label>
+                                        <input type='text' className='form-control'></input>
+
+                                        <label className='form-lable mt-3'>Message*</label>
+                                        <textarea className='form-control'></textarea>
+                                      
+                                        <ButtonStyle type='button' className='mt-4'>Send</ButtonStyle>
+                                    </form>
+                                </div>
+                            </div>
+
+                            <div className='col-lg-6'>
+
+                            </div>
                         </div>
                     </div>
                 </div>
